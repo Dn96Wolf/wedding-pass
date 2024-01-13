7287bbcf827b54c586f2f8507c9c958705c77f9e
DanBambuTeam <daniel.sanchez@bambu-mobile.com>
2024-01-12 22:24:04 -0600

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PhotoComponent } from './photo/photo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, PhotoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'boda-app';
+  title = 'my-wedding-passes';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { PhotoComponent } from './photo/photo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'wedding-pass',
+    component: PhotoComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'wedding-pass',
+    pathMatch: 'full',
+  },
+];
